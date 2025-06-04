7bf1ee305ba7e0160ed5bcf4eca6e8a40cfa3dcc
azizkefi <azizkefi5000@yahoo.fr>
2025-06-04 17:18:54 +0200

    fixesss

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="w-full top-0 z-90 bg-[#FAF9F6 ] sticky  flex items-center justify-center  shadow-md">
+    <nav className="w-full top-0 z-50 bg-[#FAF9F6 ] sticky  flex items-center justify-center  shadow-md">
       <Image
         src="/gpt.png"
         alt="Logo"
