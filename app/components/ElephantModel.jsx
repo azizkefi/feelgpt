3f00d24de57fc37f4fb92f20c0a431a466f4d78d
azizkefi <azizkefi5000@yahoo.fr>
2025-06-04 15:01:20 +0200

    animated elephant

--- app/components/ElephantModel.jsx
+++ app/components/ElephantModel.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+import { useGLTF, useAnimations ,} from '@react-three/drei'
 
 export default function ElephantModel(props) {
-  const gltf = useLoader(GLTFLoader, '/models/elephant.glb', (loader) => {
-    const dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderPath('/draco/') // Put your decoder files here
-    loader.setDRACOLoader(dracoLoader)
-  })
+  const group = useRef()
 
-  return <primitive object={gltf.scene} {...props} />
+  // Load the animated elephant (model + animation already baked)
+  const { scene, animations } = useGLTF('/models/HappyElephant.glb')
+  const { actions } = useAnimations(animations, group)
+
+  useEffect(() => {
+    scene.traverse(obj => {
+      obj.frustumCulled = false // avoid disappearing when off-screen
+    })
+
+    // Play the first animation
+    if (actions && Object.keys(actions).length > 0) {
+      const firstAction = actions[Object.keys(actions)[0]]
+      firstAction.reset().play()
+    }
+  }, [actions, scene])
+
+  return <primitive ref={group} object={scene} {...props} />
 }
